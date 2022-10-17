a0fa01148ecc8c18c843d33ba010a78ba3f929b2
Somesh-Pillai <57011503+Somesh-Pillai@users.noreply.github.com>
2022-10-17 20:18:44 +0530

    DMARC checking added

---
+++ email.checker.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain,hasMX,hasSPF,sprRecord,hasDMARC,dmarcRecord\n")
+
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+	
+}
+
+func checkDomain(domain string){
+	var hasSPF, hasDMARC, hasMX bool
+	var spfRecord, dmarcRecord string
+
+	//checking for MX records to know the email server for the domain
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 { 
+		hasMX = true
+	}
+
+	//checking for TXT records to know the authorisation of sender from the domain
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("Error:%v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	//checking for DMARC records to know authorisation of sender from the domain after checking SPF
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error:%v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+}
